22cf474205371af351656f647321dba60fc12264
ZhengjunHUO <firelouiszj@hotmail.com>
2022-06-04 16:24:58 +0200

    introduce viper into the project

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,12 +1,29 @@
 package config
 
 import (
+	"os"
+	"log"
+
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var PathToConfigFile string
 
 func ParseConfig(*cobra.Command, []string) {
+	// Check if the file exist
+	if _, err := os.Stat(PathToConfigFile); err != nil {
+                log.Fatalln(err)
+        }
+
+	// Read content into viper
+	viper.SetConfigFile(PathToConfigFile)
+	log.Println("[DEBUG] Read: ", PathToConfigFile)
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("[DEBUG] TO IMPLEMENT")
 	// TO IMPLEMENT
 	// Use Viper to read config file
 }
